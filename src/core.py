8e4c09da7f84aa61477057cfc067023f850c8fae
m1guelperez <migi.perez@web.de>
2022-11-19 22:18:28 +0100

    Added individual chat replies and postgres integration for user and item storage.

--- src/core.py
+++ src/core.py
@@ -1,4 +1,3 @@
-from time import sleep
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
@@ -10,7 +9,7 @@ from telegram import Update
 from scrape import scrape_data
 from configurations import load_configfile
 from configurations import TOKEN, CHANNEL_ID
-from utils import user_exists_in_db, add_new_user
+from utils import user_exists_in_db, add_new_customer_values, parse_message
 
 ITEMS = load_configfile("./items.toml")["items"]
 LOCATION = load_configfile("./items.toml")["search_settings"]["location"]
@@ -20,15 +19,42 @@ GIADAS_LIST = ["iphone-13pro-max-256gb"]
 
 
 async def start_function(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if not user_exists_in_db(int(update.message.from_user.id)):
-        add_new_user(int(update.message.from_user.id))
     await context.bot.send_message(
-        text="Hello! This is the EbayAlerts bot.", chat_id=update.effective_chat.id
+        text="Hello! This is the EbayAlerts bot.\nTo get you started, please use the /init command and copy and fill the following message:\n"
+        + "For example like that:",
+        chat_id=update.effective_chat.id,
+    )
+    await context.bot.send_message(
+        text="/init\n"
+        + "Itemname: Gtx 1080\n"
+        + "Pricelimit: 200 \n"
+        + "Location: Köln\n"
+        + "Radius (in km): 20",
+        chat_id=update.effective_chat.id,
     )
 
 
+async def init_first_item(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not user_exists_in_db(int(update.message.from_user.id)):
+        print("User does not exist: " + str(update.message.from_user.id))
+        customer_values = parse_message(int(update.message.from_user.id), update.message.text)
+        add_new_customer_values(int(update.message.from_user.id), customer_values)
+        await context.bot.send_message(
+            text="Great it is initialized!",
+            chat_id=update.effective_chat.id,
+        )
+    else:
+        await context.bot.send_message(
+            text="You already initialized the bot!\n"
+            + "If you want to add a new item, please use the /add command.\n"
+            + "If you want to remove an item, please use the /remove command.\n"
+            + "If you need help, use the /help command.",
+            chat_id=update.effective_chat.id,
+        )
+
+
 async def send_item_notification(chat_id: int, context: ContextTypes.DEFAULT_TYPE):
-    message = ""
+    message = "Use some commands to get started!"
     await context.bot.send_message(text=message, chat_id=chat_id)
 
 
@@ -40,14 +66,36 @@ async def remove_item_from_watchlist(update: Update, context: ContextTypes.DEFAU
     return 10
 
 
+# Handler if command isn't recognized
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command."
+    )
+
+
+# TODO: Make examples for each command.
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="The following commands are available:\n"
+        + "/start - Start the bot\n"
+        + "/init - Initialize the bot\n"
+        + "/add - Add an item to the watchlist\n"
+        + "/remove - Remove an item from the watchlist",
+    )
+
+
 def main() -> None:
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start_function))
+    application.add_handler(CommandHandler("init", init_first_item))
     application.add_handler(CommandHandler("add", add_item_to_watchlist))
     application.add_handler(CommandHandler("remove", remove_item_from_watchlist))
+    application.add_handler(CommandHandler("help", help))
     application.add_handler(
         MessageHandler(filters.TEXT & (~filters.COMMAND), send_item_notification)
     )
+    application.add_handler(MessageHandler(filters.COMMAND, unknown))
     application.run_polling()
 
 
--- src/utils.py
+++ src/utils.py
@@ -19,8 +19,44 @@ class ItemFromEbay:
         self.date = date
 
 
-def load_configfile(path: str) -> MutableMapping:
-    return toml.load(path)
+class Customer:
+    chat_id: int
+    item_name: str
+    price_limit: int
+    location: str
+    radius: int
+
+    def __init__(self, chat_id: int, item_name: str, price_limit: int, location: str, radius: int):
+        self.chat_id = chat_id
+        self.item_name = item_name
+        self.price_limit = price_limit
+        self.location = location
+        self.radius = radius
+
+
+# Get values from the incoming telegram message using the /init command
+def parse_message(chat_id: int, message: str):
+    characteristics = message.splitlines()
+    itemname = (characteristics[1].split(":")[1].strip()).lower().replace(" ", "-")
+    itemname = replace_umlauts(itemname)
+    pricelimit = int(characteristics[2].split(":")[1].strip())
+    location = characteristics[3].split(":")[1].strip()
+    radius = int(characteristics[4].split(":")[1].strip())
+    return Customer(
+        chat_id=chat_id,
+        item_name=itemname,
+        price_limit=pricelimit,
+        location=location,
+        radius=radius,
+    )
+
+
+def replace_umlauts(string: str):
+    string = string.replace("ä", "ae")
+    string = string.replace("ö", "oe")
+    string = string.replace("ü", "ue")
+    string = string.replace("ß", "ss")
+    return string
 
 
 # For change offers or items without a price tag, return 0.
@@ -45,22 +81,22 @@ def item_is_relevant(price_limit: int, item: ItemFromEbay):
         return False
 
 
-def add_new_user(user_id: int):
+def add_new_customer_values(user_id: int, customer: Customer):
     conn = connect_to_db()
     cur = conn.cursor()
     cur.execute(
-        """INSERT INTO info (chat_id,item_name,price_limit,location,radius)
-        VALUES (%s,%s,%s,%s);""",
-        (user_id, 0, "", ""),
+        """INSERT INTO customer (chat_id,item_name,item_price_limit,location,radius)
+        VALUES (%s,%s,%s,%s,%s);""",
+        (user_id, customer.item_name, customer.price_limit, customer.location, customer.radius),
     )
     conn.commit()
     close_db_connections(cur=cur, connection=conn)
 
 
-def user_exists_in_db(user_id: int):
+def user_exists_in_db(chat_id: int):
     conn = connect_to_db()
     cur = conn.cursor()
-    cur.execute("""SELECT user_id FROM users WHERE user_id = (%s);""", (user_id))
+    cur.execute("""SELECT chat_id FROM customer WHERE chat_id = (%s);""", (chat_id,))
     res_of_sql_exc = cur.fetchone()
     if res_of_sql_exc == None:
         return False
