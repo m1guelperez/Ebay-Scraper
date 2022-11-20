e297625234505edc2b6b25252e1173cfd0f47fc8
m1guelperez <migi.perez@web.de>
2022-11-20 20:07:47 +0100

    Deleted all postgres functions and places classes in class.py.

--- src/utils.py
+++ src/utils.py
@@ -2,36 +2,8 @@ from datetime import datetime
 import toml
 from typing import MutableMapping
 from postgres_utils import connect_to_db, close_db_connections
+from classes import Customer, ItemFromEbay
 
-
-class ItemFromEbay:
-    item_name: str
-    identifier: str
-    url: str
-    price: int
-    date: datetime
-
-    def __init__(self, item_name: str, identifier: str, url: str, price: int, date: datetime):
-        self.item_name = item_name
-        self.identifier = identifier
-        self.price = price
-        self.url = url
-        self.date = date
-
-
-class Customer:
-    chat_id: int
-    item_name: str
-    price_limit: int
-    location: str
-    radius: int
-
-    def __init__(self, chat_id: int, item_name: str, price_limit: int, location: str, radius: int):
-        self.chat_id = chat_id
-        self.item_name = item_name
-        self.price_limit = price_limit
-        self.location = location
-        self.radius = radius
 
 
 # Get values from the incoming telegram message using the /init command
@@ -80,29 +52,6 @@ def item_is_relevant(price_limit: int, item: ItemFromEbay):
     else:
         return False
 
-# Used as initializing for the new customer (/init command in telegram)
-def add_new_customer_values(user_id: int, customer: Customer):
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute(
-        """INSERT INTO customer (chat_id,item_name,item_price_limit,location,radius)
-        VALUES (%s,%s,%s,%s,%s);""",
-        (user_id, customer.item_name, customer.price_limit, customer.location, customer.radius),
-    )
-    conn.commit()
-    close_db_connections(cur=cur, connection=conn)
-
-
-def user_exists_in_db(chat_id: int):
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("""SELECT chat_id FROM customer WHERE chat_id = (%s);""", (chat_id,))
-    res_of_sql_exc = cur.fetchone()
-    if res_of_sql_exc == None:
-        return False
-    else:
-        return True
-
 #TODO: Finish the thread function
 def thread_scraping():
     conn = connect_to_db()
@@ -113,4 +62,4 @@ def thread_scraping():
         for item in res_of_sql_exc:
             print(str(item).replace("(", "").replace(")", "").replace(",", "").replace("'", ""))
 
-thread_scraping()
+# thread_scraping()
