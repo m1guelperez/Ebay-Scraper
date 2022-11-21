f818234ffc81fa914fc24cce18ed9bacfa89c0ce
m1guelperez <migi.perez@web.de>
2022-11-21 11:37:45 +0100

    Added list handler.

--- src/core.py
+++ src/core.py
@@ -6,17 +6,25 @@ from telegram.ext import (
 )
 import threading
 from scrape_async import *
-from telegram_command_utils import *
+from telegram_command_utils import (
+    start_function,
+    init_first_item,
+    add_item_to_watchlist,
+    remove_item_from_watchlist,
+    list_items_of_user,
+    no_command_message,
+    unknown,
+)
 from configurations import TOKEN
-from utils import parse_message
 
-
+# main method of the telegram bot
 def main_telegram_bot() -> None:
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler("start", start_function))
     application.add_handler(CommandHandler("init", init_first_item))
     application.add_handler(CommandHandler("add", add_item_to_watchlist))
     application.add_handler(CommandHandler("remove", remove_item_from_watchlist))
+    application.add_handler(CommandHandler("list", list_items_of_user))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), no_command_message))
     application.add_handler(MessageHandler(filters.COMMAND, unknown))
