f4607d9ace55ffb6b206085e93ade0fea50135a2
Miguel Perez <migi.perez@web.de>
2025-06-04 22:12:44 +0200

    Use slotted dataclasses.

--- ebayscraper/src/classes.py
+++ ebayscraper/src/classes.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from dataclasses import dataclass
 
 
+@dataclass(slots=True)
 class ItemFromEbay:
     item_name: str
     identifier: str
@@ -8,17 +10,11 @@ class ItemFromEbay:
     price: int
     date: datetime
 
-    def __init__(self, item_name: str, identifier: str, url: str, price: int, date: str):
-        self.item_name = item_name
-        self.identifier = identifier
-        self.price = price
-        self.url = url
-        self.date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-
-    def __repr__(self) -> str:
-        return f"ItemFromEbay(item_name='{self.item_name}', identifier='{self.identifier}', url='{self.url}', price={self.price}, date={self.date})"
+    def __str__(self) -> str:
+        return f"{self.item_name} ({self.identifier}) - {self.price} EUR\n{self.url}\n{self.date.strftime('%Y-%m-%d %H:%M:%S')}"
 
 
+@dataclass(slots=True)
 class Customer:
     chat_id: int
     item_name: str
@@ -26,12 +22,11 @@ class Customer:
     location: str
     radius: int
 
-    def __init__(self, chat_id: int, item_name: str, price_limit: int, location: str, radius: int):
-        self.chat_id = chat_id
-        self.item_name = item_name
-        self.price_limit = price_limit
-        self.location = location
-        self.radius = radius
-
-    def __repr__(self) -> str:
-        return f"Customer(chat_id={self.chat_id}, item_name='{self.item_name}', price_limit={self.price_limit}, location='{self.location}', radius={self.radius})"
+    def __str__(self) -> str:
+        return (
+            f"Chat ID: {self.chat_id}\n"
+            f"Item Name: {self.item_name}\n"
+            f"Price Limit: {self.price_limit} EUR\n"
+            f"Location: {self.location}\n"
+            f"Radius: {self.radius} km"
+        )
